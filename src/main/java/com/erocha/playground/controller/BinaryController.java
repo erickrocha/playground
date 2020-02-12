edac1e11cb269e5a481c43179aed7c54304f3d0f
PEREIRA DA ROCHA Erick <erick.pereira-da-rocha@ext.ec.europa.eu>
2020-02-12 18:41:03 +0100

    nois eh phoda

--- src/main/java/com/erocha/playground/controller/BinaryController.java
+++ src/main/java/com/erocha/playground/controller/BinaryController.java
@@ -4,14 +4,16 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Base64;
 
 import com.erocha.playground.model.InputJson;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
-import org.apache.tika.metadata.TikaMimeKeys;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.xml.DcXMLParser;
@@ -20,7 +22,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
 
 @RestController
@@ -35,9 +39,15 @@ public class BinaryController {
 		File file = new File("image.svg");
 		try {
 			FileUtils.writeByteArrayToFile(file, input);
+			Tika tika = new Tika();
+			String mimeType = tika.detect(file);
+			log.info(mimeType);
+			Path path = file.toPath();
+			mimeType = Files.probeContentType(path);
+			log.info(mimeType);
 			validateMimeType(inputStream);
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.error(e.getMessage(), e);
 		}
 
 	}
@@ -50,11 +60,12 @@ public class BinaryController {
 			String mediaType = metadata.get(Metadata.CONTENT_TYPE);
 			log.info(mediaType);
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.error(e.getMessage(), e);
 		} catch (SAXException e) {
-			e.printStackTrace();
+			log.error(e.getMessage(), e);
 		} catch (TikaException e) {
-			e.printStackTrace();
+			log.error(e.getMessage(), e);
+
 		}
 	}
 
@@ -63,7 +74,36 @@ public class BinaryController {
 	public void base64(@RequestBody InputJson inputJson) {
 		byte[] base64 = Base64.getMimeDecoder().decode(inputJson.getBody());
 		InputStream inputStream = new ByteArrayInputStream(base64);
-		validateMimeType(inputStream);
+		File file = new File("image.svg");
+		try {
+			FileUtils.writeByteArrayToFile(file, base64);
+			Tika tika = new Tika();
+			String mimeType = tika.detect(file);
+			log.info(mimeType);
+			Path path = file.toPath();
+			mimeType = Files.probeContentType(path);
+			log.info(mimeType);
+			validateMimeType(inputStream);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
 	}
 
+	@PostMapping
+	@RequestMapping(value = "/multipart")
+	public void multipart(@RequestParam("file") final MultipartFile upload) {
+		File file = new File("image.svg");
+		try {
+			FileUtils.writeByteArrayToFile(file, upload.getBytes());
+			Tika tika = new Tika();
+			String mimeType = tika.detect(file);
+			log.info(mimeType);
+			Path path = file.toPath();
+			mimeType = Files.probeContentType(path);
+			log.info(mimeType);
+			validateMimeType(upload.getInputStream());
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
 }
